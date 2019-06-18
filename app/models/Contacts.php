3cfd420290c303ef3019f33825fc3eff0dcb9955
IvanMejico <ivan.mejico21@gmail.com>
2019-06-18 22:17:04 +0800

    Add Contact Form

--- app/models/Contacts.php
+++ app/models/Contacts.php
@@ -1,12 +1,27 @@
 <?php
 
 class Contacts extends Model {
+    
+    public $fname, $lname, $address, $address2, $city, $state, $zip, $email, $cell_phone, $home_phone, $work_phone, $deleted = 0;
 
     public function __construct() {
         $table = 'contacts';
         parent::__construct($table);
         $this->_softDelete = true;
     }
+
+    public static $addValidation = [
+        'fname' => [
+            'display' => 'First Name',
+            'required' => true,
+            'max' => 155
+        ],
+        'lname' => [
+            'display' => 'Last Name',
+            'required' => true,
+            'max' => 155
+        ]
+    ];
 
     public function findAllByUserId($user_id, $params=[]) {
         $conditions = [
--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ define('DB_HOST', '127.0.0.1'); // database host ***use IP address to avoid DNS 
 define('DEFAULT_CONTROLLER', 'Home'); //  default_controller if there isn't one defind in the url
 define('DEFAULT_LAYOUT', 'default'); // if no layout is set in the controller use this layout.
 
-DEFINE('PROOT', '/MVC-Framework/'); // set this to '/' for a live server.
+DEFINE('PROOT', '/MVC_APP/'); // set this to '/' for a live server.
 
 define('SITE_TITLE', 'Ruah MVC Framework'); // This will be used if no site title is SITE_TITLE
 define('MENU_BRAND', 'IVAN'); // This is the Brand text in the menu
